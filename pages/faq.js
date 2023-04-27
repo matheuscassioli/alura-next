e2b1aa07cb902c49bee4404e71a2963eb4bcb674
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-04-26 21:00:25 -0300

    build fetch api FaqPage

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from '@component/componentes/Link'
 import Title from '@component/componentes/Title'
 
-export default function Faq() {
+
+export async function getStaticProps(context) {
+
+  const URL_FETCH_FAQ = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+
+  const faq = await fetch(URL_FETCH_FAQ)
+    .then((response) => {
+      return response.json()
+    })
+    .then((resposta) => {
+      return resposta
+    })
+
+  return {
+    props: {
+      faq
+    },
+  };
+}
+export default function Faq({ faq }) {
   return (
     <>
       <Title as="h1">Alura Cases - FAQ</Title>
       <Link href="/">Ir para Home</Link>
+      <ul>
+        {faq.map((k) => {
+          return <li key={k.question}>
+            <h2>{k.question}</h2>
+            <p>{k.answer}</p>
+          </li>
+        })}
+      </ul>
     </>
   )
 }
